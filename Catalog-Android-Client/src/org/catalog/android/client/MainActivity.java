e978ad8e348bc9ec18ce0eea3c1af58b11032e53
Blagovest Zlatev <blagozlatev@gmail.com>
2013-03-03 16:07:53 +0200

    Initial project and configuration

---
+++ Catalog-Android-Client/src/org/catalog/android/client/MainActivity.java
@@ -0,0 +1,13 @@
+package org.catalog.android.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
